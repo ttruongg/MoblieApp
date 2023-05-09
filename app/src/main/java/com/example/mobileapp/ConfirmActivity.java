cac9269738f734a96b278882cbdbff8e88034958
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2023-05-10 03:05:14 +0700

    complete layout

--- app/src/main/java/com/example/mobileapp/ConfirmActivity.java
+++ app/src/main/java/com/example/mobileapp/ConfirmActivity.java
@@ -2,13 +2,27 @@ package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfirmActivity extends AppCompatActivity {
+
+    Button btnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        btnFinish = (Button) findViewById(R.id.buttonFinish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_Login = new Intent(ConfirmActivity.this, HomeActivity.class);
+                startActivity(intent_Login);
+            }
+        });
     }
 }
